568dba24fe5de2b095fd11c942d10611d8c82f9e
Marvinwgnr <marvin.wagner@toulouse.miage.fr>
2023-05-10 08:26:15 +0200

    première version

---
+++ MetierEvenement.js
@@ -0,0 +1,65 @@
+// gestion des évenements
+
+// liste des évenements
+var liste = [];
+var id = 0;
+
+// Constructeur TOUS membres
+function Evenement(id,acronyme,nom_event,lieu_event,description,date_ouverture, date_cloture,nombre_max) {
+    this.id = id;
+    this.acronyme = acronyme;
+    this.nom_event = nom_event;
+    this.lieu_event = lieu_event;
+    this.description = description;
+    this.date_ouverture = date_ouverture;
+    this.date_cloture = date_cloture;
+    this.nombre_max=nombre_max;
+}
+
+// Constructeur avec structure
+function Evenement(evenement) {
+    this.id = evenement.id;
+    this.acronyme = evenement.acronyme;
+    this.nom_event = evenement.nom_event;
+    this.lieu_event = evenement.lieu_event
+    this.description = evenement.description;
+    this.date_ouverture = evenement.date_ouverture;
+    this.date_cloture = evenement.date_cloture;
+    this.nombre_max=evenement.nombre_max;
+}
+
+// METHODES METIER
+
+// Ajout
+var ajouter = function(evenement) {
+    evenement.id = id;
+    liste[id] = new Evenement(evenement);
+    id++;
+    return liste[id-1];
+}
+
+
+// Get 1 personne
+var getEvenement = function (i) {
+    if (typeof liste[i] === 'undefined') return {};
+    else return liste[i];
+}
+
+// lister les évenements
+var lister = function () {
+    return Object.values(liste);
+}
+
+//suppression
+var supprimer = function(id){
+    //supprimer l'objet dans la liste
+    var suppresion = liste.splice(id, 1); // le premier est l'index le second et combien on en supprime soit 1 seul
+    id--;
+    return suppresion;
+}
+
+exports.ajouter = ajouter;
+exports.getEvenement = getEvenement;
+exports.lister = lister;
+exports.supprimer = supprimer;
+
---
+++ app.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var bodyparser = require ("body-parser");
+var metier = require ('./MetierEvenement');
+var app = express();
+
+
+
+app.use(bodyparser.json());
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"); res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"); next();
+}); //permet depréparer la connexion avec angular
+
+
+//ajouter
+app.post('/api/evenements', function (req,res){
+    //recup les parametre
+    //si veut récupérer req.body.id ou req.body.date.... possible grâce au body parser
+    var evenement = req.body;
+
+    //Métier
+    console.log(evenement);
+    var objres = metier.ajouter(evenement);
+    //et forger le résultat au clients
+    if ((typeof objres === "undefined") || (typeof objres === {}))
+    {
+        res.status(400 ); //code 400 car erreur coté client
+    }
+    else res.status(201).json(objres);
+})
+
+//Lister
+app.get('/api/evenements', function(req,res){
+    res.status(200).json(metier.lister());
+})
+
+//Recherchern
+app.get('/api/evenements/:id', function (req,res){
+    // recup les params
+    var id = req.params.id;
+    // coté métier
+    var obj = metier.getEvenement(id);
+    //forge mle résulatt aux clients
+    if ((typeof obj === "undefined") || (typeof obj === {}))
+        res.status(404 ).json({}); //code 400 car erreur coté client
+    else res.status(201).json(obj);
+
+})
+
+app.delete('/api/evenements/:id', function(req, res){
+    //recup param
+    var id = req.params.id;
+    //coté metier
+    var objs = metier.supprimer(id);
+    //on forge res
+    if ((typeof objs === "undefined") || (typeof objs === {}))
+        res.status(404 ).json({}); //code 400 car erreur coté client
+    else res.status(201).json(objs);
+})
+
+app.listen(3000, function(){
+    console.log(('Server running...'))
+})
